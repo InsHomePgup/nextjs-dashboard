92e4f8b09fce68ef2e0b2a8012737ea1b4452b62
GeekAb <47906083+InsHomePgup@users.noreply.github.com>
2025-01-06 15:59:03 +0800

    chapter4:link

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
         <>
-            <div>invoices Page</div>
+            <div>Customers Page</div>
         </>
     );
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
         <>
-            <div>customers Page</div>
+            <div>Invoices Page</div>
         </>
     );
 }
